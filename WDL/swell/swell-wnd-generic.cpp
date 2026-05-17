f5a5f72d1dca019a85d49b5e0d38dfeba378d257
Robbie Murray <rob@mur.org.uk>
2026-05-17 22:44:03 +0100

    swell: refresh tree accessibility on expansion

--- WDL/swell/swell-wnd-generic.cpp
+++ WDL/swell/swell-wnd-generic.cpp
@@ -5461,6 +5461,7 @@ struct treeViewState
             if (!SendMessage(GetParent(hwnd),WM_NOTIFY,hwnd->m_id,(LPARAM)&nmhdr))
             {
               m_sel->m_state &= ~TVIS_EXPANDED;
+              swell_accesskit_window_changed(hwnd);
               return 2;
             }
             else
@@ -5493,6 +5494,7 @@ struct treeViewState
             if (!SendMessage(GetParent(hwnd),WM_NOTIFY,hwnd->m_id,(LPARAM)&nmhdr))
             {
               m_sel->m_state |= TVIS_EXPANDED;
+              swell_accesskit_window_changed(hwnd);
               return 2;
             }
             else
@@ -5797,6 +5799,7 @@ static LRESULT treeViewWindowProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lPa
                 if (!SendMessage(GetParent(hwnd),WM_NOTIFY,hwnd->m_id,(LPARAM)&nmhdr))
                 {
                   hit->m_state ^= TVIS_EXPANDED;
+                  swell_accesskit_window_changed(hwnd);
                   InvalidateRect(hwnd,NULL,FALSE);
                 }
                 return 0;
@@ -8062,6 +8065,7 @@ BOOL TreeView_Expand(HWND hwnd, HTREEITEM item, UINT flag)
   else if (flag == TVE_COLLAPSE) item->m_state &= ~TVIS_EXPANDED;
   else if (flag == TVE_TOGGLE) item->m_state ^= TVIS_EXPANDED;
   
+  swell_accesskit_window_changed(hwnd);
   InvalidateRect(hwnd,NULL,FALSE);
   return TRUE;
 }
